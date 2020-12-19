04f7cd6bc8766f03193e6acd152d1e47a86ca1bb
zhao <820913569@qq.com>
2020-12-19 11:43:50 +0800

    2020.12.19

--- leetcode/48.py
+++ leetcode/48.py
@@ -1,27 +1,65 @@
+# 给定一个 n × n 的二维矩阵表示一个图像。
+# 将图像顺时针旋转 90 度。
+# 说明：
+# 你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+
+
 class Solution:
     def rotate(self, matrix) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        height = len(matrix)
-        if height <= 1:
-            return matrix
-        number = (height + 1) // 2
-        for i in range(number):
-            temp = matrix[i][i: height - i]
-            for j in range(i, height-i):
-                matrix[i][height-j-1] = matrix[j][i]
-            for j in range(i, height-i):
-                matrix[j][i] = matrix[height-i-1][j]
-            for j in range(i, height-i):
-                matrix[height-i-1][j] = matrix[height-j-1][height-i-1]
-            for j in range(len(temp)-1, -1, -1):
-                matrix[i+j][height-i-1] = temp[j]
-        return matrix
+        length = len(matrix)
+        if length <= 1:
+            return
+        top, botton, left, right = 0, length - 1, 0, length - 1
+        while top < botton and left < right:
+            temp = matrix[top].copy()
+            for i in range(top, botton):
+                matrix[top][length - 1 - i] = matrix[i][left]
+            for i in range(left, right):
+                matrix[i][left] = matrix[botton][i]
+            for i in range(top + 1, botton + 1):
+                matrix[botton][length - 1 - i] = matrix[i][right]
+                matrix[i][right] = temp[i]
+            top += 1
+            botton -= 1
+            left += 1
+            right -= 1
+
+
+# 方法2 水平翻转+对角线翻转
+# class Solution:
+#     def rotate(self, matrix) -> None:
+#         length = len(matrix)
+#         if length <= 1:
+#             return
+#         for i in range(length // 2):
+#             for j in range(length):
+#                 matrix[i][j], matrix[length - 1 - i][j] = matrix[length - 1 - i][j], matrix[i][j]
+#         for i in range(length):
+#             for j in range(i):
+#                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+
+# 思路和方法1类似
+# class Solution:
+#     def rotate(self, matrix) -> None:
+#         height = len(matrix)
+#         if height <= 1:
+#             return
+#         number = (height + 1) // 2
+#         for i in range(number):
+#             temp = matrix[i][i: height - i]
+#             for j in range(i, height-i):
+#                 matrix[i][height-j-1] = matrix[j][i]
+#             for j in range(i, height-i):
+#                 matrix[j][i] = matrix[height-i-1][j]
+#             for j in range(i, height-i):
+#                 matrix[height-i-1][j] = matrix[height-j-1][height-i-1]
+#             for j in range(len(temp)-1, -1, -1):
+#                 matrix[i+j][height-i-1] = temp[j]
 
 
 if __name__ == '__main__':
     s = Solution()
-    # matrix = [[ 5, 1, 9,11],[ 2, 4, 8,10],[13, 3, 6, 7],[15,14,12,16]]
-    matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    matrix = [[ 5, 1, 9,11],[ 2, 4, 8,10],[13, 3, 6, 7],[15,14,12,16]]
+    # matrix = [[1,2,3],[4,5,6],[7,8,9]]
     print(s.rotate(matrix))
--- leetcode/930.py
+++ leetcode/930.py
@@ -1,19 +1,39 @@
 # 在由若干 0 和 1  组成的数组 A 中，有多少个和为 S 的非空子数组。
 
-from collections import Counter
+#方法1 前缀和+哈希表
+from collections import defaultdict
 class Solution(object):
     def numSubarraysWithSum(self, A, S):
-        P = [0]
-        for x in A:
-            P.append(P[-1] + x)
-        count = Counter()
+        res = 0
+        array = [0]
+        s = defaultdict(int)
+        for ele in A:
+            array.append(array[-1] + ele)
+        for ele in array:
+            res += s[ele - S]
+            s[ele] += 1
+        return res
 
-        ans = 0
-        for x in P:
-            ans += count[x]
-            count[x + S] += 1
 
-        return ans
+#方法2 找出所有1的位置，满足中间有S个1的子数组左右扩展，左边满足条件的有m个，右边满足的有n个，则以当前位置为子数组起始位置的有m*n个满足的
+class Solution:
+    def numSubarraysWithSum(self, A, S: int) -> int:
+        res = 0
+        array = [-1]
+        for i, ele in enumerate(A):
+            if ele == 1:
+                array.append(i)
+        array.append(len(A))
+        if S == 0:
+            for i in range(len(array) - 1):
+                temp = array[i + 1] - array[i] - 1
+                res += (1 + temp) * temp // 2
+        else:
+            for i in range(1, len(array) - S):
+                left = array[i] - array[i - 1]
+                right = array[i + S] - array[i + S - 1]
+                res += left * right
+        return res
 
 
 if __name__ == '__main__':
